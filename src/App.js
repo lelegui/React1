529f88293a0bc366ef2d7fe693216ea7651fac26
lelegui <lea.gui99@gmail.com>
2021-01-06 17:20:51 +0100

    popup_react

--- src/App.js
+++ src/App.js
@@ -2,30 +2,43 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component
- {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state= {
-      counter: 0,
-    };
- } 
- render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{this.state.counter}</p>
+    this.state = {value: ''};
 
-        <button
-          onClick={() => this.setState({counter: this.state.counter + 1})}
-          >
-            Click me
-          </button>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }S
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Votre texte : ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+      <body>
+        <h1>BONJOUR !</h1>
+        <h2>Saisissez un texte et cliquez sur OK pour l'afficher !</h2>
+      </body>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="OK"/>
+      </form>
       </header>
-    </div>
-  );
- }
+      </div>
+    );
+  }
 }
 
 export default App;
